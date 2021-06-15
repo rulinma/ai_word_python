4669e3f9990f4026301d0f4b5913a8dc0cde8ad0
rulinma <rulinma@gmail.com>
2021-06-15 21:59:45 +0800

    add main

---
+++ test.py
@@ -0,0 +1,18 @@
+__author__ = 'rollin'
+
+# import os
+# import time
+import datetime
+
+print('Hello World!')
+print('Time is ', datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S %A'))
+print('__name__ value: ', __name__)
+
+
+def main():
+    print('this message is from main function')
+
+
+if __name__ == '__main__':
+    main()
+    # print(__name__)
